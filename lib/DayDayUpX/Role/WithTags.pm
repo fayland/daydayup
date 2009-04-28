ea395f5c1aeb65650bcc9b458b9e3ccbb549a0d4
fayland <fayland@gmail.com>
2009-04-28 13:35:32 +0800

    bug fix

--- lib/DayDayUpX/Role/WithTags.pm
+++ lib/DayDayUpX/Role/WithTags.pm
@@ -2,17 +2,16 @@ use MooseX::Declare;
 
 # c-p http://github.com/jrockway/pleasurechicken/tree/master
 role DayDayUpX::Role::WithTags {
+    use DayDayUpX::Types qw(Set);
+    use KiokuDB::Util qw(weak_set);
     
     our $VERSION = '0.91';
-    
-    use Set::Object;
-    use MooseX::Types::Moose qw(Object);
 
     has 'tag_set' => (
         is       => 'ro',
-        isa      => Object,
+        isa      => Set,
         required => 1,
-        default  => sub { Set::Object->new },
+        default  => sub { weak_set() },
         handles  => {
             'tags'    => 'members',
             'add_tag' => 'insert',
