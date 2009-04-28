e763494b2e57782cc6a96385d8a8dd13c9673ad2
fayland <fayland@gmail.com>
2009-04-28 11:24:20 +0800

    uuid is \w

--- lib/DayDayUp.pm
+++ lib/DayDayUp.pm
@@ -47,7 +47,7 @@ class DayDayUp extends Mojolicious is mutable {
         my $r = $self->routes;
     
         # route
-        $r->route('/notes/:id/:action', id => qr/\d+/)
+        $r->route('/notes/:id/:action', id => qr/\w+/)
           ->to(controller => 'notes', action => 'index');
     
         # Default route
