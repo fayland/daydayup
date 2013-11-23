7a52eeb4da35f6db8d36758b654b8ff1395d1ce6
Fayland Lam <fayland@gmail.com>
2013-11-23 10:50:22 +0800

    fix for enum

--- old_v0_mojo/lib/DayDayUpX/Note.pm
+++ old_v0_mojo/lib/DayDayUpX/Note.pm
@@ -3,7 +3,7 @@ package DayDayUpX::Note; # make CPAN happy
 use MooseX::Declare;
 
 class DayDayUpX::Note with DayDayUpX::Role::WithTags {
-    
+
     our $VERSION = '0.93';
 
     use MooseX::Types::Moose qw(Str Int);
@@ -14,14 +14,14 @@ class DayDayUpX::Note with DayDayUpX::Role::WithTags {
         isa => Str,
         required => 1,
     );
-    
-    enum 'NoteStatus' => qw(open closed rejected suspended);
+
+    enum 'NoteStatus' => [qw(open closed rejected suspended)];
     has 'status' => (
         is  => 'rw',
         isa => 'NoteStatus',
         default => 'open',
     );
-    
+
     has 'time' => ( is => 'rw', isa => Int, required => 1 );
     has 'closed_time' => ( is => 'rw', isa => Int, default => 0 );
 };
